748be882affc05fd7435d0e4312c9aa07daa580b
Ger Hobbelt <ger@hobbelt.com>
2025-08-04 03:22:44 +0200

    fixes for monolithic build mode

--- monolithic_main.c
+++ monolithic_main.c
@@ -7,9 +7,9 @@
 #include "monolithic_main_internal_defs.h"
 
 MONOLITHIC_CMD_TABLE_START()
-	{ "light_test", {.fa = bs_threadpool_light_test_main } },
-	{ "test", {.fa = bs_threadpool_test_main } },
-	{ "maniac", {.fa = bs_threadpool_maniacal_test_main } },
+	{ "light_test", {.f = bs_threadpool_light_test_main } },
+	{ "test", {.f = bs_threadpool_test_main } },
+	{ "maniac", {.f = bs_threadpool_maniacal_test_main } },
 MONOLITHIC_CMD_TABLE_END();
 
 #include "monolithic_main_tpl.h"
